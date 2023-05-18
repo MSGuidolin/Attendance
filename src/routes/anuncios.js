e9134a4323e46ffef7965898c9343a9ce9377b4c
Marcos <cacoxg96@gmail.com>
2023-05-18 20:37:09 -0300

    roles y abm anuncios

--- src/routes/anuncios.js
+++ src/routes/anuncios.js
@@ -7,7 +7,6 @@ const { authRole,isLoggedIn } = require('../lib/auth');
 router.get('/add', isLoggedIn, authRole('Profesor'),  async (req, res) => {
     const cursos = await pool.query('SELECT * FROM cursos WHERE profesor = ?', [req.user.id]);
     const tipo = await pool.query('SELECT * FROM tipoAnuncios');
-    console.log(cursos)
     res.render('anuncios/add', {cursos, tipo});
 });
 
@@ -22,6 +21,45 @@ router.post('/add', async (req, res) => {
     };
     await pool.query('INSERT INTO anuncios set ?', [nuevoAnuncio]);
     req.flash('success', 'Anuncio guardado correctamente');
+    res.redirect('/')
+});
+
+router.get('/edit/:id', isLoggedIn, authRole('Profesor'), async (req, res) => {
+    const cursos = await pool.query('SELECT * FROM cursos WHERE profesor = ?', [req.user.id]);
+    const anuncios = await pool.query('SELECT * FROM anuncios WHERE id = ?', [req.params.id]);
+    const tipos = await pool.query('SELECT * FROM tipoAnuncios');
+    const selectedCurso = cursos.map((cursos) => {
+        if (cursos.id == anuncios[0].curso) {
+            cursos.selected = true;
+        }
+        return cursos;
+    });
+    const selectedTipo = tipos.map((tipos) => {
+        if (tipos.id == anuncios[0].tipo) {
+            tipos.selected = true;
+        }
+        return tipos;
+    });
+    console.log(anuncios[0].descripcion);
+    res.render('anuncios/edit', { anuncio: anuncios[0], cursos: selectedCurso, selectedTipo, tipos })
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { titulo, tipo, descripcion, curso} = req.body;
+    const nuevoAnuncio = {
+        titulo,
+        tipo,
+        descripcion,
+        curso
+    };
+    await pool.query('UPDATE anuncios set ? WHERE id = ?', [nuevoAnuncio, req.params.id]);
+    req.flash('success', 'Anuncio editado correctamente');
+    res.redirect('/');
+});
+
+router.get('/delete/:id', isLoggedIn, authRole('Profesor'), async (req, res) => {
+    await pool.query('DELETE FROM anuncios WHERE id =?', [req.params.id]);
+    req.flash('success', 'Anuncio eliminado correctamente');
     res.redirect('/')
 });
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,10 @@ const pool = require('../database');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+    const permiso = await pool.query('SELECT rol FROM usuarios WHERE id = ? AND rol = "Profesor"' ,[req.user.id]);
     const anuncios = await pool.query('SELECT anuncios.*, cursos.nombre AS curso, CONCAT(usuarios.nombre, " ",usuarios.apellido) profesor, tipoanuncios.tipo AS tipo FROM anuncios INNER JOIN usuarios ON usuarios.id = anuncios.profesor INNER JOIN cursos ON cursos.id = anuncios.curso INNER JOIN tipoAnuncios ON tipoAnuncios.id = anuncios.tipo INNER JOIN asistenciaCursos ON asistenciaCursos.curso = cursos.id WHERE asistenciaCursos.alumno = ? OR cursos.profesor = ?', [req.user.id, req.user.id]);
     console.log(anuncios);
-    res.render('index', {anuncios});
+    res.render('index', {anuncios, permiso});
 });
 
 module.exports = router; 
